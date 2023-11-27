a198a48e366a5f7dee462431049e67fb5c2f4330
geraldkevinjove <geraldkevinjove@gmail.com>
2023-11-27 13:11:43 +0800

    Reworked simple pokemon battles from scratch

--- index.js
+++ index.js
@@ -1,116 +1,135 @@
-let pokemonBattle = {
+let pikachuPickFriendly = document.getElementById('pikachuFriendly');
+let geodudePickFriendly = document.getElementById('geodudeFriendly');
+let mewtwoPickFriendly = document.getElementById('mewtwoFriendly');
+let pikachuPickEnemy = document.getElementById('pikachuEnemy');
+let geodudePickEnemy = document.getElementById('geodudeEnemy');
+let mewtwoPickEnemy = document.getElementById('mewtwoEnemy');
+let battleLog = document.getElementById('battleLog');
+let battleSection = document.getElementById('battleSection');
+let startBattleButton = document.getElementById('startBattle');
+let newBattleHealth = document.createElement('h3');
+let newBattleAttack = document.createElement('h3');
+let newBattleResult = document.createElement('h1');
+let isFainted = document.createElement('h2');
+let pickOne = document.getElementById('pokemon1');
+let pickTwo = document.getElementById('pokemon2');
+let pikachuPokemon;
+let geodudePokemon;
+let mewtwoPokemon;
+let pokemonPick = null;
+let pokemonFriendly;
+let pokemonEnemy;
+let isPokemonFriendlyTurn =  true;
+let pokemonFriendlyHealth;
+let pokemonEnemyHealth;
 
-    attacks:  function(name, level) {
-        this.name = name;
-        this.level = level;
-        this.health = level * 2;
-        this.attack = level;
-        this.ability = [
-            this.tackle = function tackle(target) {
-                let attack = level;
-                target.health = target.health - attack;
-                console.log(name + ' has Tackled ' + target.name);
-                console.log(target.name + ' lost ' + attack + ' HP');
-                console.log('Remaining health for ' + target.name + ' is ' + target.health);
-                if (target.health <= 0 && target.health >= -99){
-                    console.log(target.name + ' has fainted.');
-                    console.log('STOP IT IS ALREADY DEAD!')
-                } else if (target.health <= -100){
-                    console.log("I HOPE YOU'RE PROUD OF YOURSELF. PSYCHO!");
-                };
-            },
-            this.slash = function slash(target) {
-                let attack = Math.floor(Math.random() * level);
-                target.health = target.health - attack;
-                console.log(name + ' has Slashed ' + target.name);
-                console.log(target.name + ' lost ' + attack + ' HP')
-                console.log('Remaining health for ' + target.name + ' is ' + target.health);;
-                if (target.health <= 0 && target.health >= -99){
-                    console.log(target.name + ' has fainted.');
-                    console.log('STOP IT IS ALREADY DEAD!')
-                } else if (target.health <= -100){
-                    console.log("I HOPE YOU'RE PROUD OF YOURSELF. PSYCHO!");
-                };
-            },
-            this.hyperBeam = function hyperBeam(target) {
-                let attack = Math.floor(Math.random() * level);
-                target.health = target.health - attack;
-                console.log(name + ' used Hyper Beam on ' + target.name);
-                console.log(target.name + ' lost ' + attack+ ' HP');
-                console.log('Remaining health for ' + target.name + ' is ' + target.health);
-                if (target.health <= 0 && target.health >= -99){
-                    console.log(target.name + ' has fainted.');
-                    console.log('STOP IT IS ALREADY DEAD!')
-                } else if (target.health <= -100){
-                    console.log("I HOPE YOU'RE PROUD OF YOURSELF. PSYCHO!");
-                };
-            },
-            this.bodySlam = function bodySlam(target) {
-                let attack = Math.floor(Math.random() * level);
-                console.log(name + ' used Body Slam on ' + target.name);
-                target.health = target.health - attack;
-                console.log(target.name + ' lost ' + attack + ' HP');
-                console.log('Remaining health for ' + target.name + ' is ' + target.health);
-                if (target.health <= 0 && target.health >= -99){
-                    console.log(target.name + ' has fainted.');
-                    console.log('STOP IT IS ALREADY DEAD!')
-                } else if (target.health <= -100){
-                    console.log("I HOPE YOU'RE PROUD OF YOURSELF. PSYCHO!");
-                };
-            },
-            this.doubleEdge = function doubleEdge(target) {
-                let attack = Math.floor(Math.random() * level);
-                target.health = target.health - attack;
-                console.log(name + ' used Double-Edge on ' + target.name);
-                console.log(target.name + ' lost ' + attack + ' HP');
-                console.log('Remaining health for ' + target.name + ' is ' + target.health);
-                if (target.health <= 0 && target.health >= -99){
-                    console.log(target.name + ' has fainted.');
-                    console.log('STOP IT IS ALREADY DEAD!')
-                } else if (target.health <= -100){
-                    console.log("I HOPE YOU'RE PROUD OF YOURSELF. PSYCHO!");
-                };
-            }
-        ];
+// Friendly
+
+pikachuPickFriendly.addEventListener('click', function(){
+    pikachuPokemon = new Pokemon('Pikachu', 24);
+    pokemonPick = pikachuPokemon;
+    pokemonFriendlyHealth = pokemonPick.health;
+    pickOne.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonFriendlyHealth;
+    pickOne.value = pokemonPick.name;
+    pokemonFriendly = pokemonPick;
+    console.log(pickOne.value)
+    console.log(pikachuPokemon);
+})
+geodudePickFriendly.addEventListener('click', function(){
+    geodudePokemon = new Pokemon('Geodude', 28);
+    pokemonPick = geodudePokemon;
+    pokemonFriendlyHealth = pokemonPick.health;
+    pickOne.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonFriendlyHealth;
+    pickOne.value = pokemonPick.name;
+    pokemonFriendly = pokemonPick;
+    console.log(pickOne.value)
+    console.log(geodudePokemon);
+})
+mewtwoPickFriendly.addEventListener('click', function(){
+    mewtwoPokemon = new Pokemon('MewTwo', 84);
+    pokemonPick = mewtwoPokemon;
+    pokemonFriendlyHealth = pokemonPick.health;
+    pickOne.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonFriendlyHealth;
+    pickOne.value = pokemonPick.name;
+    pokemonFriendly = pokemonPick;
+    console.log(pickOne.value)
+    console.log(mewtwoPokemon);
+})
+
+// Enemy
+
+pikachuPickEnemy.addEventListener('click', function(){
+    pikachuPokemon = new Pokemon('Pikachu', 100);
+    pokemonPick = pikachuPokemon;
+    pokemonEnemyHealth = pokemonPick.health;
+    pickTwo.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonEnemyHealth;
+    pickTwo.value = pokemonPick.name;
+    pokemonEnemy = pokemonPick;
+    console.log(pickTwo.value)
+    console.log(pikachuPokemon);
+})
+geodudePickEnemy.addEventListener('click', function(){
+    geodudePokemon = new Pokemon('Geodude', 100);
+    pokemonPick = geodudePokemon;
+    pokemonEnemyHealth = pokemonPick.health;
+    pickTwo.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonEnemyHealth;
+    pickTwo.value = pokemonPick.name;
+    pokemonEnemy = pokemonPick;
+    console.log(pickTwo.value)
+    console.log(geodudePokemon);
+})
+mewtwoPickEnemy.addEventListener('click', function(){
+    mewtwoPokemon = new Pokemon('MewTwo', 100);
+    pokemonPick = mewtwoPokemon;
+    pokemonEnemyHealth = pokemonPick.health;
+    pickTwo.textContent = pokemonPick.name + ' level: ' + pokemonPick.level + ' attack: ' + pokemonPick.attack + ' health: ' + pokemonEnemyHealth;
+    pickTwo.value = pokemonPick.name;
+    pokemonEnemy = pokemonPick;
+    console.log(pickTwo.value)
+    console.log(mewtwoPokemon);
+})
+
+function Pokemon(name, level){
+    this.name = name;
+    this.level = Math.floor(Math.random() * level);
+    this.health = this.level * 2;
+    this.attack = this.level;
+    
+    this.tackle = function(target){
+        battleLog.textContent = this.name + ' has tackled ' + target.name;
+        target.health = target.health - Math.floor(Math.random() * this.attack);
+        battleSection.appendChild(newBattleAttack);
+        newBattleAttack.textContent = this.name + ' has inflicted ' + this.attack + ' damage.';
+        battleSection.appendChild(newBattleHealth);
+        newBattleHealth.textContent = target.name + ' has remaining ' + target.health + ' HP';
+        console.log(this.name + ' '+ this.health);
+        console.log(target.name + ' ' + target.health);
+        if(target.health <= 0 ){
+            target.faint();
+            battleSection.removeChild(newBattleHealth);
+            battleSection.appendChild(newBattleResult);
+            alert(newBattleResult.textContent = this.name + ' has won!');
+            battleSection.innerHTML = '';
+        }
+    };
+
+    this.faint = function(name){
+        battleSection.appendChild(isFainted);
+        isFainted.textContent = this.name + ' has fainted!';
     }
 };
 
-let pokemonRecords = {
-    name: [],
-    level: []
-};
+startBattleButton.addEventListener('click', function(){
 
-// let pikachu = new pokemonBattle.attacks('Pikachu', 10);
-// console.log(pikachu);
-// let geodude = new pokemonBattle.attacks('Geodude', 15);
-// console.log(geodude);
+    if (pokemonFriendly !== undefined && pokemonEnemy !== undefined){
+        if(isPokemonFriendlyTurn){
+            pokemonFriendly.tackle(pokemonEnemy);
+        } else {
+            pokemonEnemy.tackle(pokemonFriendly);
+        }
+        isPokemonFriendlyTurn = !isPokemonFriendlyTurn;
+    } else {
+        alert('Please choose your pokemons!');
+    }
 
-alert('Welcome to my simple pokemon battle sims. I will still keep updating this program for practice.');
-alert('Please open console log window to see the battles.')
-
-let randomAttack = pokemonBattle.attacks();
-let randomAbility = pokemonBattle.ability;
-const randomValue = [Math.floor(randomAbility.length * Math.random())]
-
-let newFriendlyPokemon = prompt('Enter new friendly Pokemon name:');
-let newFriendlyPokemonLevel = Number(prompt('Enter new friendly Pokemon level:'));
-let newEnemyPokemon = prompt('Enter new enemy Pokemon name');
-let newEnemyPokemonLevel = Number(prompt('Enter new enemy Pokemon level:'));
-
-pokemonRecords.name.push(newFriendlyPokemon);
-// console.log(pokemonRecords.name);
-pokemonRecords.name.push(newEnemyPokemon);
-// console.log(pokemonRecords.name);
-pokemonRecords.level.push(newFriendlyPokemonLevel);
-// console.log(pokemonRecords.level);
-pokemonRecords.level.push(newEnemyPokemonLevel);
-// console.log(pokemonRecords.level);
-
-let friendlyPokemon = new pokemonBattle.attacks(newFriendlyPokemon, newFriendlyPokemonLevel);
-let enemyPokemon = new pokemonBattle.attacks(newEnemyPokemon, newEnemyPokemonLevel);
-
-
-// Initialize battle
-
-friendlyPokemon.ability[randomValue](enemyPokemon);
-console.warn('use friendlyPokemon.ability[randomValue](enemyPokemon) to continue battle');
+});
